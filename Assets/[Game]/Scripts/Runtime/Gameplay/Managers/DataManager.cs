fd2441c9fd90ef7fe176b76ae3230a227e8a3a22
mabiş <43499212+ladylovelacee@users.noreply.github.com>
2025-01-30 00:25:18 +0300

    Update DataManager.cs

--- Assets/[Game]/Scripts/Runtime/Gameplay/Managers/DataManager.cs
+++ Assets/[Game]/Scripts/Runtime/Gameplay/Managers/DataManager.cs
@@ -6,5 +6,15 @@ namespace Runtime.Gameplay
     public class DataManager : Singleton<DataManager>
     {
         [field : SerializeField] public ColorContainer ColorContainer {  get; private set; }
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            ColorContainer.InitializeContainer();
+        }
     }
 }
