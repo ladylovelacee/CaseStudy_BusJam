b609536064196cebeaf8c116cd777283f8cee3c8
mabiş <43499212+ladylovelacee@users.noreply.github.com>
2025-01-30 13:24:58 +0300

    Create instance container

--- Assets/[Game]/Scripts/Runtime/Gameplay/Managers/DataManager.cs
+++ Assets/[Game]/Scripts/Runtime/Gameplay/Managers/DataManager.cs
@@ -6,6 +6,7 @@ namespace Runtime.Gameplay
     public class DataManager : Singleton<DataManager>
     {
         [field : SerializeField] public ColorContainer ColorContainer {  get; private set; }
+        [field: SerializeField] public InstanceContainer InstanceContainer { get; private set; } = new();
 
         private void Awake()
         {
@@ -16,5 +17,13 @@ namespace Runtime.Gameplay
         {
             ColorContainer.InitializeContainer();
         }
+    }
+
+    [System.Serializable]
+    public class InstanceContainer
+    {
+        [field: SerializeField] public PassengerBase Passenger { get; private set; }
+        [field: SerializeField] public VehicleBase Vehicle { get; private set; }
+        [field: SerializeField] public GridCell Cell { get; private set; }
     }
 }
