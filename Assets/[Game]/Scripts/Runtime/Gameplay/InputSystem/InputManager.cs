f88e226f404f60699d1d6b60f006054af8a7d91a
mabiş <43499212+ladylovelacee@users.noreply.github.com>
2025-02-01 23:49:38 +0300

    Remove new input system

--- Assets/[Game]/Scripts/Runtime/Gameplay/InputSystem/InputManager.cs
+++ Assets/[Game]/Scripts/Runtime/Gameplay/InputSystem/InputManager.cs
@@ -1,39 +1,24 @@
 using Runtime.Core;
+using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Runtime.Gameplay
 {
     public class InputManager : Singleton<InputManager>
     {
-        private TouchControls touchControls;
-        public TouchControls TouchControls => touchControls ??= new TouchControls();
+        public event Action OnTapDown;
+        public event Action OnTapUp;
 
-        /// <summary>
-        /// Shows if the finger is currently being held down.
-        /// </summary>
-        public bool IsFingerDown { get; private set; }
-
-        /// <summary>
-        /// Shows the current <b>screen position</b> of finger.
-        /// </summary>
-        public Vector2 CurrentFingerPosition { get; private set; }
-
-        private void OnEnable()
+        private void Update()
         {
-            TouchControls.Enable();
-            TouchControls.Touch.TouchPosition.performed += HandleFingerPosition;
-        }
-
-        private void OnDisable()
-        {
-            TouchControls.Disable();
-            TouchControls.Touch.TouchPosition.performed -= HandleFingerPosition;
-        }
-
-        private void HandleFingerPosition(InputAction.CallbackContext context)
-        {
-            CurrentFingerPosition = TouchControls.Touch.TouchPosition.ReadValue<Vector2>();
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnTapDown?.Invoke();
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                OnTapUp?.Invoke();
+            }
         }
     }
 }
